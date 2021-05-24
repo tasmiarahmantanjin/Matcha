67c784397f3bbbac9e84ebad3244f0c6a275f330
pontuslaandersson <pontuslaandersson@github.com>
2021-05-24 16:10:04 +0300

    Corrected comment.

--- client/src/components/Auth/ForgotPassword.js
+++ client/src/components/Auth/ForgotPassword.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import './Auth.scss';
 import { useDispatch } from 'react-redux'
-// import the login action
+// import the forgotPassword action
 import { forgotPassword } from '../../store/actions/auth'
 
 
