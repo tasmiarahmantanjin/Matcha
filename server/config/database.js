d6413a69cad5aef698eaf3d9a951215fe73afe87
Tasmia Rahman <tasmiata@gmail.com>
2021-08-02 18:17:00 +0300

    draft code for retry logic in db_pool

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const config = require('./app')
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: config.appDbHost,
+  host: config.appDbHost || 'localhost',
   user: config.appDbUser,
   password: config.appDbPass,
   port: 5432,
@@ -10,3 +10,30 @@ const pool = new Pool({
 });
 
 module.exports = pool;
+
+// Code to add retry logic
+// const pgPool = new Pool(config);
+// const pgPoolWrapper = {
+//   async connect() {
+//     for (let nRetry = 1; ; nRetry++) {
+//       try {
+//         const client = await pgPool.connect();
+//         if (nRetry > 1) {
+//           console.info('Now successfully connected to Postgres');
+//         }
+//         return client;
+//       } catch (e) {
+//         if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
+//           console.info('ECONNREFUSED connecting to Postgres, ' +
+//             'maybe container is not ready yet, will retry ' + nRetry);
+//           // Wait 1 second
+//           await new Promise(resolve => setTimeout(resolve, 1000));
+//         } else {
+//           throw e;
+//         }
+//       }
+//     }
+//   }
+// };
+
+// module.exports = pool;
